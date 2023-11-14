b7b0d605593f82229ec870b97fbbb9b7c6b1ee34
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-11-14 10:59:22 +0800

    Add files via upload

---
+++ ASForLoops.py
@@ -0,0 +1,11 @@
+for i in range (4): # print i, (auto:start 0), end 3, (auto:step 1)
+    print(i)
+
+for i in range (0,4): # print i, start 0, end 3, (auto:step 1)
+    print(i)
+
+for i in range (10,20,2): # print i, start 0, end 20, step 2
+    print(i)
+
+# this is how for loop works
+# end must be minus 1
