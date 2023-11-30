3cd4944be75f16551f53cc109e46f9bee48b8cc1
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-11-30 18:06:16 +0800

    Update ASInitializearray.py

--- ASInitializearray.py
+++ ASInitializearray.py
@@ -1,15 +1,15 @@
-i=int(0)
-j=int(0)
-List = [0 for i in range(7)]
+List1 = [0 for i in range(7)] # value 0 
+
+j = 0
 for i in range (0,7):
-    Num= int(input("Please input a number: "))
-    List[j]=Num
+    Num = int(input("Please input a number: "))
+    List[j] = Num
     j=j+1
-print(List)
+print(List1)
 
 #or
 
-List2 = []
+List2 = [] # empty list
 for i in range (0,7):
     List2.append(int(input("Please enter num")))
 print(List2)
