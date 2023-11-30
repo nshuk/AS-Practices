35ec0a55d209b9e01b76c3ac620c0dc49f5e4100
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-11-30 18:11:15 +0800

    Update and rename ASNumbers.py to ASString&Numbers.py

---
+++ ASString&Numbers.py
@@ -0,0 +1,12 @@
+string1 = "ABCDEFG HIJKLM"
+print(string1[3:9])
+
+string2="5"
+print(int(string2)) # converts numerical string into integer
+
+num1 = (float(input("enter a number with a decimal value"))) # use of float if you expect a real number
+num2 = num1 + 3
+print (num1)
+
+print(int(2.7))
+# it truncates, not round off to the nearest 1
