716a6d71aa7dd4a5d84f4198e2de5485f45e58eb
naqib shukri <118101646+nshuk@users.noreply.github.com>
2023-12-01 08:30:25 +0800

    Update ASForLoops.py

--- ASForLoops.py
+++ ASForLoops.py
@@ -4,7 +4,7 @@ for i in range (4): # print i, (auto:start 0), end 3, (auto:step 1)
 for i in range (0,4): # print i, start 0, end 3, (auto:step 1)
     print(i)
 
-for i in range (10,20,2): # print i, start 0, end 20, step 2
+for i in range (10,21,2): # print i, start 10, end 20, step 2
     print(i)
 
 # this is how for loop works
