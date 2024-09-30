396be568dc83bfdd0fc938d0a830240cb78a98bc
naqib shukri <118101646+nshuk@users.noreply.github.com>
2024-09-30 12:32:39 +0100

    Update ASInitializearray.py

--- ASInitializearray.py
+++ ASInitializearray.py
@@ -3,7 +3,7 @@ List1 = [0 for i in range(7)] # value 0
 j = 0
 for i in range (0,7):
     Num = int(input("Please input a number: "))
-    List[j] = Num
+    List1[j] = Num
     j=j+1
 print(List1)
 
